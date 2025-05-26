13bd9bba750bcb26ac73defc17b540c931415253
Adiiity <69859467+Adiiity@users.noreply.github.com>
2025-05-25 20:24:49 -0400

    Time: 11 ms (71.6%), Space: 26.3 MB (69.2%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -12,19 +12,15 @@ class Solution(object):
         """
 
         self.res=0
-
-        def dfs(curr):  
+        def dfs(curr):
             if not curr:
                 return 0
-
+            
             left=dfs(curr.left)
             right=dfs(curr.right)
 
-            self.res=max(self.res, right+left)
-
+            self.res=max(self.res, left+right)
             return 1+max(left,right)
-
         dfs(root)
-
         return self.res
         
