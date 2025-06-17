96f03eab786e213d235786270f1b3943fc1ad6b5
Adiiity <69859467+Adiiity@users.noreply.github.com>
2025-06-17 13:45:20 -0400

    Time: 82 ms (40.45%), Space: 13.4 MB (74.36%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -11,21 +11,23 @@ class Solution(object):
         :type subRoot: Optional[TreeNode]
         :rtype: bool
         """
+       
         if not t:
             return True
         if not s:
             return False
-        
-        if self.sameTree(s,t):
+
+        if self.isSameTree(s,t):
             return True
-        return (self.isSubtree(s.left,t) or
-                self.isSubtree(s.right,t))
-        
-    def sameTree(self, s,t):
+        else:
+            return (self.isSubtree(s.left,t) or self.isSubtree(s.right,t))
+
+    def isSameTree(self,s,t):
         if not s and not t:
             return True
+        
         if s and t and s.val==t.val:
-            return (self.sameTree(s.left,t.left) and 
-                    self.sameTree(s.right,t.right))
-            
-        return False
+            return (self.isSameTree(s.left, t.left) and 
+                    self.isSameTree(s.right,t.right))
+        else:
+            return False
