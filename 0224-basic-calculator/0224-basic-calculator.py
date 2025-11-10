b3dbb5cc9e102b8481e8855a921d17314387bc37
Adiiity <69859467+Adiiity@users.noreply.github.com>
2025-11-10 18:40:27 -0500

    Time: 55 ms (48.95%), Space: 18.4 MB (75.2%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        # example 1+(2-(3+4))
+        sign=1
+        res=0
+        num=0
+        stack=[]
+        for ch in s:
+            if ch.isdigit():
+                num=num*10+int(ch)
+            if ch=='(':
+                stack.append(res)
+                stack.append(sign)
+                res=0
+                sign=1
+            elif ch in '+-':
+                res+=sign*num
+                num=0
+                sign=1 if ch=='+' else -1
+            elif ch==')':
+                res+=sign*num
+                num=0
+                res*=stack.pop()
+                res+=stack.pop()
+        
+        res+=num*sign
+        return res
+
+
+        
