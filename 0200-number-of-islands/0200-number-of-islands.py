caf82f711141bef50e6030d6ce9a656380d73cb6
Adiiity <69859467+Adiiity@users.noreply.github.com>
2025-11-11 16:46:51 -0500

    Time: 319 ms (8.61%), Space: 25.4 MB (21.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,60 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # if not grid:
-        #     return 0
-
-        # rows,cols=len(grid),len(grid[0])
-        # visited=set()
-        # islands=0
-
-        # def bfs(r,c):
-        #     q=collections.deque()
-        #     visited.add((r,c))
-        #     q.append((r,c))
-        #     while q:
-        #         row,col=q.popleft()
-
-        #         directions=[[1,0],[-1,0],[0,1],[0,-1]]
-
-        #         for dr,dc in directions:
-        #             r=dr+row
-        #             c=dc+col
-        #             if (r in range(rows) and
-        #                 c in range(cols) and 
-        #                 grid[r][c]=="1" and
-        #                 (r,c) not in visited):
-        #                 visited.add((r,c))
-        #                 q.append((r,c))
-
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if grid[r][c]=="1" and (r,c) not in visited:
-        #             bfs(r,c)
-        #             islands+=1
-        # return islands
-
-        if not grid: 
-            return 0
-
-        islands=0
-        rows,cols=len(grid),len(grid[0])
+        if not grid:
+            return 0 
         
-        def dfs(r,c):
-            if (r<0 or c<0 or
-                r>=rows or c>=cols or 
-                grid[r][c]=="0"):
-                return
+        rows,cols=len(grid),len(grid[0])
+        visit=set()
+        islands=0
 
-            grid[r][c]="0"
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
+        def bfs(r,c):
+            q=deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q: 
+                row,col=q.popleft()
+                directions=[[1,0],[0,1],[-1,0],[0,-1]]
+                for dr,dc in directions:
+                    r=row+dr
+                    c=col+dc
+                    if (r in range(rows) and c in range(cols) and grid[r][c]=="1" and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=="1":
-                    dfs(r,c)
+                if grid[r][c]=="1" and (r,c) not in visit:
+                    bfs(r,c)
                     islands+=1
-
         return islands
+        
