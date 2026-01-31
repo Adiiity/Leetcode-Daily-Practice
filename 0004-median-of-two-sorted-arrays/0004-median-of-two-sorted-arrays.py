2d6743a3ea0ad60aa21c5c8d32caf6b46469eaa3
Adiiity <69859467+Adiiity@users.noreply.github.com>
2026-01-30 23:02:20 -0500

    Time: 1 ms (61.29%), Space: 19.7 MB (20.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,32 +1,37 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1)>len(nums2):
-            nums1,nums2=nums2,nums1
         
-        total=len(nums1)+len(nums2)
+        A,B=nums1,nums2
+        if len(A)>len(B):
+            A,B=B,A
+        
+        l,r=0,len(A)-1
+
+        total=len(A)+len(B)
         half=total//2
 
-        A,B=nums1,nums2
-        l,r=0,len(A)-1
-        while True: 
-            i=(l+r)//2
+        while True:
+            i=(l+r)//2 #for A
             j=half-i-2
 
             Aleft=A[i] if i>=0 else float("-infinity")
             Aright=A[i+1] if (i+1)<len(A) else float("infinity")
+
             Bleft=B[j] if j>=0 else float("-infinity")
             Bright=B[j+1] if (j+1)<len(B) else float("infinity")
 
-            #  if partition is correct
+            # correct partition
             if Aleft<=Bright and Bleft<=Aright:
-                # odd
+                # if even
                 if total%2:
                     return min(Aright,Bright)
-                
-                # even
-                return ((max(Aleft,Bleft)+min(Aright,Bright))/2)
-            # not correct
-            elif Aleft>Bright:
-                r=i-1
-            else:
+                return (max(Aleft,Bleft)+min(Aright,Bright))/2
+                    
+            # if wrong partition
+            elif Bleft>Aright:
                 l=i+1
+            else:
+                r=i-1
+            
+
+
