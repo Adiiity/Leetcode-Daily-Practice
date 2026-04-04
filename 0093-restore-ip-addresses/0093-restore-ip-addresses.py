e8e5c7ecb2c614e3f0f6b3a8380180345872c39d
Adiiity <69859467+Adiiity@users.noreply.github.com>
2026-04-04 16:45:54 -0500

    Time: 0 ms (100%), Space: 19.4 MB (77.8%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,22 +1,23 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        
-        res=[]
+        # valid combos in any order - Backtracking 
+        output=[]
 
-        def backtrack(i,dots,curIP):
-
-            # think of base case 
-            if dots==4 and len(s)==i:
-                res.append(curIP[:-1])
-                return
+        def backtrack(i, dots, curIP):
+            # base cases
+            if dots==4 and i==len(s):
+                return output.append(curIP[:-1])
             if dots>4:
-                return
-
-            # backtrack loop
+                return 
+            
             for j in range(i,min(i+3,len(s))):
-                if int(s[i:j+1])< 256 and (i==j or s[i]!="0"):
-                    backtrack(j+1,dots+1,curIP + s[i:j+1] + ".")
+                if int(s[i:j+1]) <256 and (i==j or s[i]!="0"):
+                    backtrack(j+1,dots+1, curIP+s[i:j+1]+'.')
             
+        
         backtrack(0,0,"")
-        return res
+        return output
             
+            
+
+        
